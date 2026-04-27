d4b6768b66a84698741101ffa7f607222e425791
davposmar <davposmar@alum.us.es>
2026-04-27 19:19:40 +0200

    feat: get manager realm and edit data
    - add services and controllers related to authenticated realm to get
    manager role.
    - update menu with manager option

---
+++ src/main/java/acme/features/authenticated/manager/AuthenticatedManagerCreateService.java
@@ -0,0 +1,83 @@
+/*
+ * 
+ * Copyright (C) 2012-2026 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.authenticated.manager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.components.principals.Authenticated;
+import acme.client.components.principals.UserAccount;
+import acme.client.helpers.PrincipalHelper;
+import acme.client.services.AbstractService;
+import acme.realms.Manager;
+
+@Service
+public class AuthenticatedManagerCreateService extends AbstractService<Authenticated, Manager> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AuthenticatedManagerRepository	repository;
+
+	private Manager							manager;
+
+	// AbstractService interface -------------------------------------------
+
+
+	@Override
+	public void load() {
+		int userAccountId;
+		UserAccount userAccount;
+
+		userAccountId = super.getRequest().getPrincipal().getAccountId();
+		userAccount = this.repository.findOneUserAccountById(userAccountId);
+
+		this.manager = super.newObject(Manager.class);
+		this.manager.setUserAccount(userAccount);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status;
+
+		status = !super.getRequest().getPrincipal().hasRealmOfType(Manager.class);
+
+		super.setAuthorised(status);
+	}
+
+	@Override
+	public void bind() {
+		super.bindObject(this.manager, "position", "skills", "executive");
+	}
+
+	@Override
+	public void validate() {
+		super.validateObject(this.manager);
+	}
+
+	@Override
+	public void execute() {
+		this.repository.save(this.manager);
+	}
+
+	@Override
+	public void unbind() {
+		super.unbindObject(this.manager, "position", "skills", "executive");
+	}
+
+	@Override
+	public void onSuccess() {
+		if (super.getRequest().getMethod().equals("POST"))
+			PrincipalHelper.handleUpdate();
+	}
+
+}
